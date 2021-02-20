460f3f56eb7e67a1e0cad73ee88b8367b6f76b27
SEO-J17 <qw4643987@naver.com>
2021-02-20 23:18:19 +0900

    02-20

---
+++ Programmers/src/Level2/다리를지나는트럭.java
@@ -0,0 +1,5 @@
+package Level2;
+
+public class 다리를지나는트럭 {
+
+}
---
+++ Programmers/src/Level2/주식가격.java
@@ -0,0 +1,9 @@
+package Level2;
+
+public class 주식가격 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
