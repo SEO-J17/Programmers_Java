07463709d31548cf1e270df67a4eea44e329b5f9
SEO-J17 <qw4643987@naver.com>
2021-02-25 21:07:05 +0900

    02-25

--- Programmers/src/Level1/신규아이디추천.java
+++ Programmers/src/Level1/신규아이디추천.java
@@ -65,3 +65,4 @@ public class 신규아이디추천 {
 //substring을 이용할 때 숫자로 써주는 거 보다 길이 예외가 생기지 않게 length를 이용해서 자를것.
 //startsWith,endsWith는 String에서 처음,끝의 어떤 문자가 들어 있는지 true,false로 반환 해준다.
 //소문자로 변환할땐 toLowerCase(), 대문자로 변환 할땐 toUpperCase()를 이용해라.
+//charAt이용하여 String으로 넣을땐 String.valueOf를 사용해서 넣을것.
