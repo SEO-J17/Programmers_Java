07801446164c78134b7ac8e99539639a83f1d992
SEO-J17 <qw4643987@naver.com>
2021-03-04 23:45:30 +0900

    03-04

--- Programmers/src/Level1/모의고사.java
+++ Programmers/src/Level1/모의고사.java
@@ -1,10 +1,13 @@
 package Level1;
+
+import java.util.ArrayList;
+
 public class 모의고사 {
 	class Solution {
-	    public int[] solution(int[] answers) {
+	    public ArrayList <Integer> solution(int[] answers) {
 	        int[] rank = new int [3];
 	        String [] person = new String [3];
-	        int[] answer = new int [3];
+	        ArrayList <Integer> answer = new ArrayList<>();
 	        person [0] = "12345";
 	        person [1] = "21232425";
 	        person [2] = "3311224455";
@@ -12,33 +15,30 @@ public class 모의고사 {
 	        for(int i=0; i<3; i++){
 	            int index=0;
 	            for(int j=0; j<answers.length; j++){
-	                if(index < person[i].length()){
-	                    if(Integer.valueOf(person[i].charAt(index)) == answers[j]){
+	                    if(Integer.valueOf(person[i].charAt(index)-'0') == answers[j]){
 	                        rank[i]++;
 	                        index++;
 	                    }else
 	                        index++;
-	                }
+	                                
 	                if(index >= person[i].length())
 	                    index = 0;                
 	            }
 	        }
 	        
-	        int max=0;
-	        for(int i=0; i<2; i++){
-	            if(rank[i] > rank[i+1]){
+	        int max=-1;
+	        for(int i=0; i<3; i++){
+	            if(max < rank[i]){
 	                max = rank[i];
-	            }else if(rank[i] < rank[i+1]){             
-	                max = rank[i+1];                                                           
-	            }
+	            }                          
 	        }
 	        
-	        if(max == rank[0]) answer[0] = 1;
-	        if(max == rank[1]) answer[1] = 2;
-	        if(max == rank[2]) answer[2] = 3;
+	        if(max == rank[0]) answer.add(1);
+	        if(max == rank[1]) answer.add(2);
+	        if(max == rank[2]) answer.add(3);
 	    
 	        return answer;
 	    }
-	
 	}
 }
+//String에서 Integer로 바꿀땐 무조건 '0'을 빼줘야 한다. 아스키코드로 변형된다!!!!
