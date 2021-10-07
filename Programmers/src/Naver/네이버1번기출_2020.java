59c96bab72771adeb899b5927f98ce31e0b67db4
SEO <qw4643987@naver.com>
2021-10-07 11:11:30 +0900

    10-07 solved

---
+++ Programmers/src/Naver/네이버1번기출_2020.java
@@ -0,0 +1,56 @@
+//제일 처음에 1차원 배열로 선언하고 정렬해서 풀었음.. 너무 비효율적이었음
+//최대,최소값을 찾고 최대,최소값 둘 중하나가 target이랑 같고 chk가 1이하면 제외했음
+package Naver;
+
+import java.io.*;
+import java.util.*;
+
+public class 네이버1번기출_2020 {
+	class Solution {
+		public String solution(int[][] scores) {
+			String answer = "";
+			for (int i = 0; i < scores.length; i++) {
+				int target = scores[i][i];
+				int chk = 0;
+				int avg = 0;
+				int sum = 0;
+				int min = 999;
+				int max = -1;
+				int divide = scores[i].length;
+				for (int j = 0; j < scores[i].length; j++) {
+					int score = scores[j][i];
+					if (target == score)
+						chk++;
+
+					max = Math.max(score, max);
+					min = Math.min(score, min);
+
+					sum += score;
+				}
+
+				if (target == min || target == max) {
+					if (chk <= 1) {
+						sum -= target;
+						--divide;
+					}
+				}
+
+				avg = sum / divide;
+
+				if (avg >= 90) {
+					answer += "A";
+				} else if (avg >= 80 && avg < 90) {
+					answer += "B";
+				} else if (avg >= 70 && avg < 80) {
+					answer += "C";
+				} else if (avg >= 50 && avg < 70) {
+					answer += "D";
+				} else if (avg < 50) {
+					answer += "F";
+				}
+			}
+
+			return answer;
+		}
+	}
+}
