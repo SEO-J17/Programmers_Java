10e4c888fd996b558246ab389198d75d79f815cb
SEO-J17 <qw4643987@naver.com>
2022-01-28 01:16:06 +0900

    01-28 solved

--- Programmers/src/Level1/신고결과받기_2022.java
+++ Programmers/src/Level1/신고결과받기_2022.java
@@ -7,33 +7,32 @@ public class 신고결과받기_2022 {
 	class Solution {
 		public int[] solution(String[] id_list, String[] report, int k) {
 			int[] answer = new int[id_list.length];
-			ArrayList<String> list[] = new ArrayList[id_list.length];
+
+			HashMap<String, List<String>> list = new HashMap<>();
 			HashMap<String, Integer> map = new HashMap<>();
 
 			for (int j = 0; j < id_list.length; j++) {
 				map.put(id_list[j], 0);
+				list.put(id_list[j], new ArrayList());
 			}
 
-			for (int i = 0; i < id_list.length; i++) {
-				list[i] = new ArrayList();
-				for (int j = 0; j < report.length; j++) {
-					StringTokenizer st = new StringTokenizer(report[j]);
-					String str1 = st.nextToken();
-					String str2 = st.nextToken();
-					if (id_list[i].equals(str1)) {
-						if (!list[i].contains(str2)) {
-							list[i].add(str2);
-							int temp = map.get(str2);
-							map.put(str2, ++temp);
-						}
-					}
+			for (int j = 0; j < report.length; j++) {
+				StringTokenizer st = new StringTokenizer(report[j]);
+				String str1 = st.nextToken();
+				String str2 = st.nextToken();
+				if (!list.get(str1).contains(str2)) {
+					list.get(str1).add(str2);
+					int temp = map.get(str2);
+					map.put(str2, ++temp);
 				}
+
 			}
 
 			for (int i = 0; i < id_list.length; i++) {
 				int cnt = 0;
-				for (int j = 0; j < list[i].size(); j++) {
-					if (map.get(list[i].get(j)) >= k) {
+				String temp = id_list[i];
+				for (int j = 0; j < list.get(temp).size(); j++) {
+					if (map.get(list.get(temp).get(j)) >= k) {
 						++cnt;
 					}
 				}
