130ce0f8a51a723a9be9f4b3352fd0fbf219fb36
SEO-J17 <qw4643987@naver.com>
2022-04-28 15:35:43 +0900

    04-28 solved

---
+++ Programmers/src/Level2/더맵게.java
@@ -0,0 +1,33 @@
+package Level2;
+
+import java.io.*;
+import java.util.*;
+
+public class ´õ¸Ê°Ô {
+	class Solution {
+		public int solution(int[] scoville, int K) {
+			PriorityQueue<Integer> qu = new PriorityQueue<>();
+			int answer = 0;
+
+			for (int i = 0; i < scoville.length; i++) {
+				qu.add(scoville[i]);
+			}
+
+			while (qu.peek() <= K) {
+				if (qu.size() <= 1) {
+					answer = -1;
+					break;
+				}
+				++answer;
+
+				int min1 = qu.poll();
+				int min2 = qu.poll();
+				int mix = min1 + (min2 * 2);
+				qu.add(mix);
+
+			}
+
+			return answer;
+		}
+	}
+}
