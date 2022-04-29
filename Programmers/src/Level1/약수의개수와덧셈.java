a7f0f50bfae7e012b1575be77023b786353c79c3
SEO-J17 <qw4643987@naver.com>
2022-04-29 15:44:17 +0900

    04-29 solved

---
+++ Programmers/src/Level1/약수의개수와덧셈.java
@@ -0,0 +1,28 @@
+package Level1;
+
+public class ¾à¼öÀÇ°³¼ö¿Íµ¡¼À {
+	class Solution {
+	    public int solution(int left, int right) {
+	        int answer = 0;
+	        int even = 0;   //Â¦¼ö
+	        int odd = 0;    //È¦¼ö
+	        
+	        for(int i = left; i <=right; i++){
+	            int num = 0;
+	            for(int j = 1; j <= i; j++){
+	                if(i % j == 0){
+	                    num++;
+	                }
+	            }
+	            if(num % 2 == 0){
+	                even += i;
+	            }else
+	                odd -= i;
+	        }
+	        
+	        answer = even + odd;
+	        
+	        return answer;
+	    }
+	}
+}
